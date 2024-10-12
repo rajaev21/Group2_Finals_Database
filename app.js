3d5e5586e8e7ca38675288c95b5c14147077644b
rajeav21 <rajaevberame21@gmail.com>
2024-10-12 17:11:16 +0800

    added

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(3000, ()=> {
+    console.log('3000');
+})
